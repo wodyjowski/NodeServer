9f9a72f38112b2c418ea59a458994f89f8c3a4ca
Michał Kisiel <mkisiel58@gmail.com>
2019-08-07 23:58:17 +0200

    changeeees

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var http = require('http');
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 90;
+var number = 0;
 
 var server = http.createServer(function(req, res) {
-    res.write('xxxxx');
+    res.write('xxxxx ' + ++numer);
     res.end();
 });
 
