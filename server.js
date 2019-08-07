9a39a8585648f6e72fcf491245d5c96c9094fc8d
Michał Kisiel <mkisiel58@gmail.com>
2019-08-08 00:11:48 +0200

    log requests

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ var port = process.env.PORT || 90;
 var number = 0;
 
 var server = http.createServer(function(req, res) {
+    console.log(req);
     res.write('xxxxx ' + ++number);
-    res.end();2
+    res.end();
 });
 
 
