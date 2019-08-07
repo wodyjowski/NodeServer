1267641fa30bc4f05d80c6de7c05e8ab6fd47a66
Michał Kisiel <mkisiel58@gmail.com>
2019-08-07 23:14:51 +0200

    test 2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var port = process.env.PORT || 80;
 
 var server = http.createServer(function(req, res) {
-    res.write('heroku looks very promising');
+    res.write('heroku looks very promising xd');
     res.end();
 });
 
