003e7459acccc0c51a447b617e3d34a1a6ec7d48
Michał Kisiel <mkisiel58@gmail.com>
2019-08-08 00:09:01 +0200

    quick fix

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var port = process.env.PORT || 90;
 var number = 0;
 
 var server = http.createServer(function(req, res) {
-    res.write('xxxxx ' + ++numer);
-    res.end();
+    res.write('xxxxx ' + ++number);
+    res.end();2
 });
 
 
