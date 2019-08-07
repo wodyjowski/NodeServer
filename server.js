cb81ae6a184c3a38670663a58bd8de20a81f4056
Michał Kisiel <mkisiel58@gmail.com>
2019-08-07 23:11:11 +0200

    text change to see if heroku reflects changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var port = process.env.PORT || 80;
 
 var server = http.createServer(function(req, res) {
-    res.write('siemka');
+    res.write('heroku looks very promising');
     res.end();
 });
 
