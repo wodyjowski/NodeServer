0ca951c367b78bb0121b76cff32907eb3e1f94a1
mkisiel <mkisiel@micomp.pl>
2019-08-08 16:21:45 +0200

    g

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
-var http = require('http');
-var port = process.env.PORT || 90;
-var number = 0;
+const express = require('express');
+var pug = require('pug');
+var bodyParser = require('body-parser')
 
-var server = http.createServer(function(req, res) {
-    console.log(req);
-    res.write('xxxxx ' + ++number);
-    res.end();
+var mustacheExpress = require('mustache-express');
+const app = express();
+
+app.engine('html', mustacheExpress());
+app.use(express.static('public'))
+app.set('view engine', 'html');
+app.use(bodyParser.urlencoded({ 
+    extended: true
+})); 
+
+const port = process.env.PORT || 80;
+
+var messageList = []
+
+app.post('/', (req, res) =>
+{
+    messageList.unshift({name: req.body.name, message: req.body.message});
+    res.header('Location', '/');
+    res.redirect('/');
 });
 
+app.get('/', (req, res) =>
+{
+    res.render('index', {messageList})
+});
 
-server.listen(port);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
