cb7af3722d0793c62ee8be59c600d55baf098bef
mkisiel <mkisiel@micomp.pl>
2019-08-09 17:00:45 +0200

    first attempt at postgresql

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const express = require('express');
-var pug = require('pug');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+var Sequelize = require('sequelize');
 
 var mustacheExpress = require('mustache-express');
 const app = express();
+
+const sequelize = new Sequelize('lech', 'postgres', 'sa', {
+    // gimme postgres, please!
+    dialect: 'postgres'
+  })
+
+  class User extends Sequelize.Model {}
+User.init({
+  username: Sequelize.STRING,
+  birthday: Sequelize.DATE
+}, { sequelize, modelName: 'user' });
+
+sequelize.sync()
+  .then(() => User.create({
+    username: 'janedoe',
+    birthday: new Date(1980, 6, 20)
+  }))
+  .then(jane => {
+    console.log(jane.toJSON());
+  });
+
 
 app.engine('html', mustacheExpress());
 app.use(express.static('public'))
