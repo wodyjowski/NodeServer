c6b1ae0237f3fa3c8e7707865ef782f9c72fce63
Michał Kisiel <mkisiel58@gmail.com>
2019-08-10 15:34:42 +0200

    async messages

---
+++ public/code.js
@@ -0,0 +1,43 @@
+var socket = new WebSocket("ws://" + location.host);
+
+socket.onopen = function (e) {
+    console.log("Connection established");
+};
+
+socket.onmessage = function (event) {
+    var data = JSON.parse(event.data);
+    console.log(`[message] Data received from server: ${data}`);
+
+    var list = document.getElementById('messagesLs');
+
+    var li = document.createElement('li');
+    li.innerHTML = "<b>" + data.name +":</b> " + data.message;
+    list.appendChild(li);
+};
+
+socket.onerror = error => {
+    console.log(`WebSocket error: ${error}`);
+    location.reload();
+};
+
+
+function formToJSON(form) {
+    var inputs = form.getElementsByTagName('input');
+    var object = {};
+
+    for (let index = 0; index < inputs.length; index++) {
+        const element = inputs[index];
+        object[element.getAttribute('name')] = element.value;
+    }
+
+    return object;
+}
+
+function sendData() {
+    var form = document.getElementById('messageForm');
+    var obj = formToJSON(form);
+    var json = JSON.stringify(obj);
+
+    console.log(json);
+    socket.send(json);
+}
--- server.js
+++ server.js
@@ -1,53 +1,54 @@
 const express = require('express');
 var bodyParser = require('body-parser');
-var Sequelize = require('sequelize');
+var WebSocket = require('ws');
+var WebSocketServer = require('ws').Server;
 
 var mustacheExpress = require('mustache-express');
 const app = express();
-
-const sequelize = new Sequelize('lech', 'postgres', 'sa', {
-    // gimme postgres, please!
-    dialect: 'postgres'
-  })
-
-  class User extends Sequelize.Model {}
-User.init({
-  username: Sequelize.STRING,
-  birthday: Sequelize.DATE
-}, { sequelize, modelName: 'user' });
-
-sequelize.sync()
-  .then(() => User.create({
-    username: 'janedoe',
-    birthday: new Date(1980, 6, 20)
-  }))
-  .then(jane => {
-    console.log(jane.toJSON());
-  });
 
 
 app.engine('html', mustacheExpress());
 app.use(express.static('public'))
 app.set('view engine', 'html');
-app.use(bodyParser.urlencoded({ 
+app.use(bodyParser.urlencoded({
     extended: true
-})); 
+}));
 
 const port = process.env.PORT || 80;
 
 var messageList = []
 
-app.post('/', (req, res) =>
-{
-    messageList.unshift({name: req.body.name, message: req.body.message});
+app.post('/', (req, res) => {
+    messageList.unshift({ name: req.body.name, message: req.body.message });
     res.header('Location', '/');
     res.redirect('/');
 });
 
-app.get('/', (req, res) =>
-{
-    res.render('index', {messageList})
+app.get('/', (req, res) => {
+    res.render('index', { messageList })
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+var server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+var wss = new WebSocketServer({ server: server });
+
+wss.on("connection", function (ws) {
+    console.log("connected", new Date);
+
+    ws.on('message', message => {
+        var obj = JSON.parse(message);
+
+        if(obj.name === '' || obj.message === '') return;
+
+        messageList.unshift({ name: obj.name, message: obj.message });
+
+        wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify(obj));
+            }
+        });
+
+        console.log(message);
+    });
+});
 
